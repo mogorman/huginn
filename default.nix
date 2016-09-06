1b09ffab52ffcd25d0e997758675f6b420af401d
Matthew O'Gorman <mog@rldn.net>
2016-09-05 23:06:06 -0400

    trying to clean up a bit

--- default.nix
+++ default.nix
@@ -6,39 +6,14 @@ let
     gemfile = ./Gemfile;
     lockfile = ./Gemfile.lock;
     gemset = ./gemset.nix;
-
-  extraCmds = ''
-  export LANG='en_US.UTF-8'
-  '';
-
-  shellHook = ''
-  export LANG='en_US.UTF-8'
-  '';
-
-
-    buildInputs = with self; [ pkgs.glibcLocales ];
-    LANG = "en_US.UTF-8";
-    LC_ALL = "en_US.UTF-8";
-
   };
 in stdenv.mkDerivation {
   name = "huginn";
-  buildInputs = [env ruby v8 curl postgresql glibcLocales ];
+  buildInputs = [env ruby v8 curl postgresql ];
   shellHook = ''
     export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
     export GEM_HOME=./rubygems
     export PATH=./rubygems/bin:$PATH
     export LD_LIBRARY_PATH=${curl}/lib:${postgresql}/lib
-    export LANG=en_US.UTF-8
-'';
-  # used when building environments
-  extraCmds = ''
-  export LANG='en_US.UTF-8'
-  '';
-  preConfigure = ''
-    echo "I AM DOING SOMETHING"
-    sleep 40
-    export LANG="en_US.UTF-8"
-    export LOCALE_ARCHIVE=$glibcLocales/lib/locale/locale-archive
 '';
 }
