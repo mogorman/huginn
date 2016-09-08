c8b479b5de61a57c33184f97ad207f2a75d7e85a
Matthew O'Gorman <mog@rldn.net>
2016-09-08 15:21:13 -0400

    cleanup

--- default.nix
+++ default.nix
@@ -11,6 +11,7 @@ in stdenv.mkDerivation {
   name = "huginn";
   buildInputs = [env ruby v8 curl postgresql ];
   shellHook = ''
+    export PS1="huginn> "
     export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
     export LD_LIBRARY_PATH=${curl.out}/lib:${postgresql}/lib
 '';
